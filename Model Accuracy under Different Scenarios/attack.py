32756ee128f1e82d8c7c7411b63dc5ff8ecddd24
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-03-26 17:17:10 +0800

    refactor(RQ3): update to tf 2

--- Model Accuracy under Different Scenarios/attack.py
+++ Model Accuracy under Different Scenarios/attack.py
@@ -14,13 +14,13 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
 
-import keras
-from keras import backend as K
+import tensorflow.keras as keras
+from tensorflow.keras import backend as K
 ## load mine trained model
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 import art
-from art.estimators.classification import KerasClassifier
+from art.estimators.classification import TensorFlowV2Classifier
 
 from art.attacks.evasion import FastGradientMethod
 from art.attacks.evasion import CarliniLInfMethod
@@ -29,7 +29,9 @@ from art.attacks.evasion import BasicIterativeMethod
 from art.attacks.evasion import SaliencyMapMethod
 from art.attacks.evasion import AutoProjectedGradientDescent
 from art.attacks.evasion import DeepFool, NewtonFool
-from art.attacks.evasion import SquareAttack, SpatialTransformation, Wasserstein
+from art.attacks.evasion import SquareAttack, SpatialTransformation
+from art.attacks.evasion import ShadowAttack, Wasserstein
+
 
 
 import tensorflow as tf
@@ -48,14 +50,16 @@ def customTime(*args):
     converted = utc_dt.astimezone(pytz.timezone("Singapore"))
     return converted.timetuple()
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 ## [original from FSE author] for solving some specific problems, don't care
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
+# config = tf.compat.v1.ConfigProto()
+# config.gpu_options.allow_growth = True
+# sess = tf.compat.v1.Session(config=config)
 
 
-VERBOSE = False
+# VERBOSE = False
+VERBOSE = True
 
 DATA_DIR = "../data/"
 MODEL_DIR = "../models/"
@@ -75,19 +79,42 @@ APGD = "apgd"
 DF = "deepfool"
 NF = "newtonfool"
 SA = "squareattack"
+SHA = "shadowattack"
 ST = "spatialtransformation"
-WA = 'wassersteinattack'
-ATTACK_NAMES = [APGD, BIM, CW, DF, FGSM, JSMA, NF, PGD, SA, ST, WA]
+WA = "wasserstein"
+ATTACK_NAMES = [APGD, BIM, CW, DF, FGSM, JSMA, NF, PGD, SA, SHA, ST, WA]
 ## Note:  already tried APGD, but it doesn't work
 
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+
+def train_step(model, images, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(images, training=True)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
 
 ## classifier paramaters
 ## depend to the dataset used
 classifier_params = {}
 for dataset_name in DATASET_NAMES :
-    classifier_params[dataset_name] = {}
-classifier_params[MNIST] = {"clip_values": (-0.5, 0.5)}
-classifier_params[SVHN] = {"clip_values": (-0.5, 0.5)}
+    classifier_params[dataset_name] = {"loss_object": loss_object, "train_step": train_step}
+
+classifier_params[MNIST]["nb_classes"] = 10
+classifier_params[MNIST]["input_shape"] = (28, 28, 1)
+classifier_params[MNIST]["clip_values"] = (-0.5, 0.5)
+
+classifier_params[CIFAR]["nb_classes"] = 10
+classifier_params[CIFAR]["input_shape"] = (32, 32, 3)
+
+classifier_params[SVHN]["nb_classes"] = 10
+classifier_params[SVHN]["input_shape"] = (32, 32, 3)
+classifier_params[SVHN]["clip_values"] = (-0.5, 0.5)
+
+
 
 ## attack parameters for generating adversarial images
 ## Note: I use the same format with FSE paper, i.e. params[<attack>][<dataset>]
@@ -102,7 +129,7 @@ attack_params = {}
 # empty means using the original parameters for ART
 attack_params[APGD] = {}
 for dataset_name in DATASET_NAMES:
-    attack_params[APGD][dataset_name] = {}
+    attack_params[APGD][dataset_name] = {"loss_type": "cross_entropy"}
 
 attack_params[CW] = {}
 for dataset_name in DATASET_NAMES :
@@ -110,11 +137,11 @@ for dataset_name in DATASET_NAMES :
 
 attack_params[DF] = {}
 for dataset_name in DATASET_NAMES:
-    attack_params[DF][dataset_name] = {}
+    attack_params[DF][dataset_name] = {"batch_size": 256}
 
 attack_params[NF] = {}
 for dataset_name in DATASET_NAMES:
-    attack_params[NF][dataset_name] = {}
+    attack_params[NF][dataset_name] = {"batch_size": 256}
 
 attack_params[JSMA] = {}
 for dataset_name in DATASET_NAMES:
@@ -124,9 +151,17 @@ attack_params[SA] = {}
 for dataset_name in DATASET_NAMES:
     attack_params[SA][dataset_name] = {}
 
+attack_params[SHA] = {}
+for dataset_name in DATASET_NAMES:
+    attack_params[SHA][dataset_name] = {"batch_size": 1}
+
 attack_params[ST] = {}
 for dataset_name in DATASET_NAMES:
     attack_params[ST][dataset_name] = {}
+
+attack_params[WA] = {}
+for dataset_name in DATASET_NAMES:
+    attack_params[WA][dataset_name] = {}
 
 
 attack_params[PGD] = {}
@@ -178,6 +213,7 @@ def call_function_by_attack_name(attack_name):
         NF: NewtonFool,
         PGD: ProjectedGradientDescent,
         SA: SquareAttack,
+        SHA: ShadowAttack,
         ST: SpatialTransformation,
         WA: Wasserstein
     }[attack_name]
@@ -188,11 +224,12 @@ def gen_adv_data(model, x, y, attack_name, dataset_name, batch_size=2048):
     logging.getLogger().setLevel(logging.CRITICAL)
     
     classifier_param = classifier_params[dataset_name]
-    classifier = KerasClassifier(model, **classifier_param)
+    classifier = TensorFlowV2Classifier(model=model, **classifier_param)
     
     attack_param = attack_params[attack_name][dataset_name]
     if attack_name not in [ST] :
-        attack_param["batch_size"] = batch_size
+        if "batch_size" not in attack_param :
+            attack_param["batch_size"] = batch_size
     if attack_name not in [FGSM, BIM] : ## some attacks don't have verbose parameter, e.g. bim
         attack_param["verbose"] = VERBOSE
     attack = call_function_by_attack_name(attack_name)(classifier, **attack_param)
@@ -233,101 +270,76 @@ def accuracy(model, x, labels):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Attack for DNN')
     parser.add_argument(
+        '--dataset', help="Model Architecture", type=str, default="mnist")
+    parser.add_argument(
+        '--model', help="Model Architecture", type=str, default="lenet1")
+    parser.add_argument(
+        '--attack', help="Adversarial examples", type=str, default="fgsm")
+    parser.add_argument(
         '--batch_size', help="batch size for generating adversarial examples", type=int, default=1024)
 
     args = parser.parse_args()
 
+    dataset_name = args.dataset
+    model_name = args.model
+    attack_name = args.attack
+    
+    ## Prepare directory for saving adversarial images and logging
+    adv_dir = "{}{}/adv/{}/{}/".format(
+        DATA_DIR, dataset_name, model_name, attack_name)
+    if not os.path.exists(adv_dir):
+        os.makedirs(adv_dir)
+    logging.basicConfig(
+        format='[%(asctime)s] - %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S',
+        level=logging.INFO,
+        handlers=[
+            logging.FileHandler(
+                os.path.join(adv_dir, 'output.log')),
+            logging.StreamHandler()
+        ])
+
     logging.Formatter.converter = customTime
     logger = logging.getLogger("adversarial_images_generation")
     
-    dataset_name = MNIST
-    model_name = "lenet1"
-    attack_name = PGD
 
-    datasets = [MNIST, CIFAR, SVHN]
+    ## Load benign images from mnist, cifar, or svhn
+    x_train, y_train, x_test, y_test = load_data(dataset_name)
 
-    # model_dict = {
-    #     MNIST: ['lenet1', 'lenet4', 'lenet5'],
-    #     CIFAR: ['vgg16', 'resnet20'],
-    #     SVHN: ['svhn_model', 'svhn_first', 'svhn_second']
-    # }
+    ## Load keras pretrained model for the specific dataset
+    model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset_name, model_name)
+    model = load_model(model_path)
+    model.summary()
     
-    model_dict = {
-        MNIST: ['lenet1']
-    }
+    logger.info("")
+    logger.info("Generating Adversarial Images")
+    logger.info("Use GPU: {}".format(len(get_available_gpus()) > 0))
+    if len(get_available_gpus()) > 0 :
+        logger.info("Available GPUs: {}".format(get_available_gpus()))
 
-    # model_dict = {
-    #     CIFAR: ['vgg16']
-    # }
+    logger.info("Dataset: {}".format(dataset_name))
+    logger.info("Model: {}".format(model_name))
+    logger.info("Attack: {}".format(attack_name))
 
-    # attack_names = ATTACK_NAMES
-    attack_names = [BIM, CW, DF, FGSM, JSMA, NF, PGD, SA, ST]
-    # attack_names = [FGSM]
-    # attack_names = [PGD]
-    # attack_names = [BIM]
-    # attack_names = [CW]
-    # attack_names = [JSMA]
-    # attack_names = [APGD]
+    ## Check the accuracy of the original model on benign images
+    acc = accuracy(model, x_test, y_test)
+    logger.info("Model accuracy on benign images: {:.2f}%".format(acc))
 
+    ## Generate adversarial images
+    x_adv = gen_adv_data(model, x_test, y_test, attack_name,
+                    dataset_name, args.batch_size)
+    
+    ## Check the accuracy of the original model on adversarial images
+    acc = accuracy(model, x_adv, y_test)
+    logger.info("Model accuracy on adversarial images: {:.2f}%".format(acc))
+    
+    ## Save the adversarial images into external file
+    x_adv_path = "{}x_test.npy".format(adv_dir)
+    np.save(x_adv_path, x_adv)
 
-    for dataset_name in datasets:
-        if dataset_name in model_dict:
-            for model_name in model_dict[dataset_name]:
-                for attack_name in attack_names :
-                    
-                    ## Prepare directory for saving adversarial images and logging
-                    adv_dir = "{}{}/adv/{}/{}/".format(
-                        DATA_DIR, dataset_name, model_name, attack_name)
-                    if not os.path.exists(adv_dir):
-                        os.makedirs(adv_dir)
-                    logging.basicConfig(
-                        format='[%(asctime)s] - %(message)s',
-                        datefmt='%Y/%m/%d %H:%M:%S',
-                        level=logging.INFO,
-                        handlers=[
-                            logging.FileHandler(
-                                os.path.join(adv_dir, 'output.log')),
-                            logging.StreamHandler()
-                        ])
+    ## Note: y_test will exactly be the same with the benign y_test
+    ##       thus it's not a must to save the y_test
+    y_adv_path = "{}y_test.npy".format(adv_dir)
+    np.save(y_adv_path, y_test)
 
-
-                    ## Load benign images from mnist, cifar, or svhn
-                    x_train, y_train, x_test, y_test = load_data(dataset_name)
-
-                    ## Load keras pretrained model for the specific dataset
-                    model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset_name, model_name)
-                    model = load_model(model_path)
-                    model.summary()
-                    
-                    logger.info("")
-                    logger.info("Generating Adversarial Images")
-                    logger.info("Use GPU: {}".format(len(get_available_gpus()) > 0))
-                    if len(get_available_gpus()) > 0 :
-                        logger.info("Available GPUs: {}".format(get_available_gpus()))
-
-                    logger.info("Dataset: {}".format(dataset_name))
-                    logger.info("Model: {}".format(model_name))
-                    logger.info("Attack: {}".format(attack_name))
-
-                    ## Check the accuracy of the original model on benign images
-                    acc = accuracy(model, x_test, y_test)
-                    logger.info("Model accuracy on benign images: {:.2f}%".format(acc))
-
-                    ## Generate adversarial images
-                    x_adv = gen_adv_data(model, x_test, y_test, attack_name,
-                                    dataset_name, args.batch_size)
-                    
-                    ## Check the accuracy of the original model on adversarial images
-                    acc = accuracy(model, x_adv, y_test)
-                    logger.info("Model accuracy on adversarial images: {:.2f}%".format(acc))
-                    
-                    ## Save the adversarial images into external file
-                    x_adv_path = "{}x_test.npy".format(adv_dir)
-                    np.save(x_adv_path, x_adv)
-
-                    ## Note: y_test will exactly be the same with the benign y_test
-                    ##       thus it's not a must to save the y_test
-                    y_adv_path = "{}y_test.npy".format(adv_dir)
-                    np.save(y_adv_path, y_test)
-
-                    logger.info("Adversarial images are saved at {}".format(adv_dir))
+    logger.info("Adversarial images are saved at {}".format(adv_dir))
