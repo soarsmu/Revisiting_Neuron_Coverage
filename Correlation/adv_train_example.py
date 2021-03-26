f627e98aa9cb36cd4ce629a3fd65a7e90056230c
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-03-26 16:51:34 +0800

    refactor(adv_train): fetch parameters from global files

--- Correlation/adv_train_example.py
+++ Correlation/adv_train_example.py
@@ -3,13 +3,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import sys
+sys.path.append('..')
+
 import os
 import argparse
-import sys, logging
+import logging
 import time
 from datetime import datetime
 import pytz
 import numpy as np
+import parameters as param
+from utils import load_data, call_function_by_attack_name
 
 import warnings
 warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
@@ -52,171 +57,8 @@ def customTime(*args):
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-## [original from FSE author] for solving some specific problems, don't care
-# config = tf.compat.v1.ConfigProto()
-# config.gpu_options.allow_growth = True
-# sess = tf.compat.v1.Session(config=config)
 
-
-# VERBOSE = False
 VERBOSE = True
-
-DATA_DIR = "../data/"
-MODEL_DIR = "../models/"
-
-MNIST = "mnist"
-CIFAR = "cifar"
-SVHN = "svhn"
-
-DATASET_NAMES = [MNIST, CIFAR, SVHN]
-
-BIM = "bim"
-CW = "cw"
-FGSM = "fgsm"
-JSMA = "jsma"
-PGD = "pgd"
-APGD = "apgd"
-DF = "deepfool"
-NF = "newtonfool"
-SA = "squareattack"
-SHA = "shadowattack"
-ST = "spatialtransformation"
-WA = "wasserstein"
-ATTACK_NAMES = [APGD, BIM, CW, DF, FGSM, JSMA, NF, PGD, SA, SHA, ST, WA]
-## Note:  already tried APGD, but it doesn't work
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
-
-
-def train_step(model, images, labels):
-    with tf.GradientTape() as tape:
-        predictions = model(images, training=True)
-        loss = loss_object(labels, predictions)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
-
-## classifier paramaters
-## depend to the dataset used
-classifier_params = {}
-for dataset_name in DATASET_NAMES :
-    classifier_params[dataset_name] = {"loss_object": loss_object, "train_step": train_step}
-
-classifier_params[MNIST]["nb_classes"] = 10
-classifier_params[MNIST]["input_shape"] = (28, 28, 1)
-classifier_params[MNIST]["clip_values"] = (-0.5, 0.5)
-
-classifier_params[CIFAR]["nb_classes"] = 10
-classifier_params[CIFAR]["input_shape"] = (32, 32, 3)
-
-classifier_params[SVHN]["nb_classes"] = 10
-classifier_params[SVHN]["input_shape"] = (32, 32, 3)
-classifier_params[SVHN]["clip_values"] = (-0.5, 0.5)
-
-
-
-## attack parameters for generating adversarial images
-## Note: I use the same format with FSE paper, i.e. params[<attack>][<dataset>]
-##       we may change the format into params[<dataset>][<attack>]
-##       to track the consistent epsilon for each dataset
-attack_params = {}
-
-## TO DO: read the paper for each attack
-##        make sure to use the correct parameters
-##        for each combination of attack and dataset
-
-# empty means using the original parameters for ART
-attack_params[APGD] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[APGD][dataset_name] = {"loss_type": "cross_entropy"}
-
-attack_params[CW] = {}
-for dataset_name in DATASET_NAMES :
-    attack_params[CW][dataset_name] = {}
-
-attack_params[DF] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[DF][dataset_name] = {"batch_size": 256}
-
-attack_params[NF] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[NF][dataset_name] = {"batch_size": 256}
-
-attack_params[JSMA] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[JSMA][dataset_name] = {}
-
-attack_params[SA] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[SA][dataset_name] = {}
-
-attack_params[SHA] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[SHA][dataset_name] = {"batch_size": 1}
-
-attack_params[ST] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[ST][dataset_name] = {}
-
-attack_params[WA] = {}
-for dataset_name in DATASET_NAMES:
-    attack_params[WA][dataset_name] = {}
-
-
-attack_params[PGD] = {}
-attack_params[PGD][MNIST] = {'eps': .3,
-                      'eps_step': .03,
-                      'max_iter': 20
-                      }
-attack_params[PGD][CIFAR] = {'eps': 16. / 255.,
-                      'eps_step': 2. / 255.,
-                      'max_iter': 30
-                      }
-attack_params[PGD][SVHN] = {'eps': 8. / 255.,
-                      'eps_step': 0.01,
-                      'max_iter': 30
-                      }
-
-# use the same epsilon used in pgd
-attack_params[BIM] = {}
-attack_params[BIM][MNIST] = {'eps': .3
-                                  }
-attack_params[BIM][CIFAR] = {'eps': 16. / 255.
-                                  }
-attack_params[BIM][SVHN] = {'eps': 8. / 255.
-                                 }
-
-
-# use the same epsilon used in pgd
-attack_params[FGSM] = {}
-attack_params[FGSM][MNIST] = {'eps': .3
-                                 }
-attack_params[FGSM][CIFAR] = {'eps': 16. / 255.
-                                 }
-attack_params[FGSM][SVHN] = {'eps': 8. / 255.
-                                }
-
-
-
-def call_function_by_attack_name(attack_name):
-    if attack_name not in ATTACK_NAMES:
-        print('Unsupported attack: {}'.format(attack_name))
-        sys.exit(1)
-    return {
-        APGD: AutoProjectedGradientDescent,
-        BIM: BasicIterativeMethod,
-        CW: CarliniLInfMethod,
-        DF: DeepFool,
-        FGSM: FastGradientMethod,
-        JSMA: SaliencyMapMethod,
-        NF: NewtonFool,
-        PGD: ProjectedGradientDescent,
-        SA: SquareAttack,
-        SHA: ShadowAttack,
-        ST: SpatialTransformation,
-        WA: Wasserstein
-    }[attack_name]
 
 
 # integrate all attack method in one function and only construct graph once
@@ -242,14 +84,7 @@ def gen_adv_data(model, x, y, attack_name, dataset_name, batch_size=2048):
 
 
 
-# the data is in range(-.5, .5)
-def load_data(dataset_name):
-    assert dataset_name in DATASET_NAMES
-    x_train = np.load(DATA_DIR + dataset_name + '/benign/x_train.npy')
-    y_train = np.load(DATA_DIR + dataset_name + '/benign/y_train.npy')
-    x_test = np.load(DATA_DIR + dataset_name + '/benign/x_test.npy')
-    y_test = np.load(DATA_DIR + dataset_name + '/benign/y_test.npy')
-    return x_train, y_train, x_test, y_test
+
 
 
 def softmax(x):
@@ -286,7 +121,7 @@ def adv_retrain(attack_name, dataset, model_name, batch_size=512):
     labels_test = np.argmax(model.predict(x_test), axis=1)
     print('Accuracy test set: %.2f%%' % (np.sum(labels_test == labels_true) / x_test.shape[0] * 100))
 
-    classifier_param = classifier_params[dataset_name]
+    classifier_param = param.classifier_params[dataset_name]
     classifier = TensorFlowV2Classifier(model=model, **classifier_param)
 
     attack_param = attack_params[attack_name][dataset_name]
@@ -357,7 +192,7 @@ if __name__ == "__main__":
     x_train, y_train, x_test, y_test = load_data(dataset)
 
     ## Load keras pretrained model for the specific dataset
-    model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset, model_name)
+    model_path = "{}{}/{}.h5".format(param.MODEL_DIR, dataset, model_name)
     model = load_model(model_path)
     model.summary()
 
@@ -366,14 +201,14 @@ if __name__ == "__main__":
     labels_test = np.argmax(model.predict(x_test), axis=1)
     print('Accuracy test set: %.2f%%' % (np.sum(labels_test == labels_true) / x_test.shape[0] * 100))
 
-    classifier_param = classifier_params[dataset_name]
+    classifier_param = param.classifier_params[dataset_name]
     classifier = TensorFlowV2Classifier(model=model, **classifier_param)
 
-    attack_param = attack_params[attack_name][dataset_name]
-    if attack_name not in [ST] :
+    attack_param = param.attack_params[attack_name][dataset_name]
+    if attack_name not in [param.ST] :
         if "batch_size" not in attack_param :
             attack_param["batch_size"] = batch_size
-    if attack_name not in [FGSM, BIM] : ## some attacks don't have verbose parameter, e.g. bim
+    if attack_name not in [param.FGSM, param.BIM] : ## some attacks don't have verbose parameter, e.g. bim
         attack_param["verbose"] = VERBOSE
     
     attack = call_function_by_attack_name(attack_name)(classifier, **attack_param)
@@ -385,10 +220,3 @@ if __name__ == "__main__":
     else:
         nb_epochs = 80
     trainer.fit(x_train, y_train, nb_epochs=nb_epochs, batch_size=512)
-
-
-
-
-
-
-
