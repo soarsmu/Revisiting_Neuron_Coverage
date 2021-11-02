a42154186985d2a87bec67b899a95bb698470fc5
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2021-11-02 22:39:44 +0800

    chore(): define some global variables

--- Coverage and Robustness VS. Training Datasets/helper.py
+++ Coverage and Robustness VS. Training Datasets/helper.py
@@ -9,6 +9,31 @@ import os
 import shutil
 from skimage.measure import compare_ssim as SSIM
 
+VERBOSE = True
+
+DATA_DIR = "../data/"
+MODEL_DIR = "../models/"
+
+MNIST = "mnist"
+CIFAR = "cifar"
+SVHN = "svhn"
+
+DATASET_NAMES = [MNIST, CIFAR, SVHN]
+
+BIM = "bim"
+CW = "cw"
+FGSM = "fgsm"
+JSMA = "jsma"
+PGD = "pgd"
+APGD = "apgd"
+DF = "deepfool"
+NF = "newtonfool"
+SA = "squareattack"
+SHA = "shadowattack"
+ST = "spatialtransformation"
+WA = "wasserstein"
+ATTACK_NAMES = [APGD, BIM, CW, DF, FGSM, JSMA, NF, PGD, SA, SHA, ST, WA]
+
 # helper function
 def get_layer_i_output(model, i, data):
     layer_model = K.function([model.layers[0].input], [model.layers[i].output])
@@ -492,14 +517,13 @@ def mutate(img):
     # Otherwise the mutation is failed. Line 20 in Algo 2
     return img_new
 
-# the data is in range(-.5, .5)
-def load_data(name):
-    assert (name.upper() in ['MNIST', 'CIFAR', 'SVHN'])
-    name = name.lower()
-    x_train = np.load('../data/' + name + '_data/' + name + '_x_train.npy')
-    y_train = np.load('../data/' + name + '_data/' + name + '_y_train.npy')
-    x_test = np.load('../data/' + name + '_data/' + name + '_x_test.npy')
-    y_test = np.load('../data/' + name + '_data/' + name + '_y_test.npy')
+
+def load_data(dataset_name):
+    assert dataset_name in DATASET_NAMES
+    x_train = np.load(DATA_DIR + dataset_name + '/benign/x_train.npy')
+    y_train = np.load(DATA_DIR + dataset_name + '/benign/y_train.npy')
+    x_test = np.load(DATA_DIR + dataset_name + '/benign/x_test.npy')
+    y_test = np.load(DATA_DIR + dataset_name + '/benign/y_test.npy')
     return x_train, y_train, x_test, y_test
 
 def softmax(x):
