cd888d8236a1ec6b0a90e3fe4adea57f9035e0da
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2021-11-02 14:32:03 +0800

    feat(RQ1): measure coverage metrics after retraining

--- Coverage and Robustness VS. Training Datasets/cycle.py
+++ Coverage and Robustness VS. Training Datasets/cycle.py
@@ -1,6 +1,7 @@
 
 
 from helper import load_data, mutate, softmax, compare_nc, retrain
+from helper import Coverage
 import argparse
 import tensorflow as tf
 import numpy as np
@@ -15,6 +16,41 @@ THIS_MODEL_DIR = "./models/"
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config=config)
+
+def evaluate_robustness(model):
+
+    pass
+
+
+def evaluate_coverage(model, l, T, x_train, y_train, x_test, y_test):
+    path_to_result = "coverage_results/{}/{}/{}".format(dataset_name, model_name, is_improve)
+    os.makedirs(path_to_result, exist_ok=True)
+
+    coverage = Coverage(model, x_train, y_train, x_test, y_test, x_train)
+    # Based on the original code to evaluate Coverage, x_train and x_train_adv is the same.
+    # In the Coverage class, only the last parameter is used.
+    nc1, activate_num1, total_num1 = coverage.NC(l, threshold=0.1)
+    nc2, activate_num2, total_num2 = coverage.NC(l, threshold=0.3)
+    nc3, activate_num3, total_num3 = coverage.NC(l, threshold=0.5)
+    nc4, activate_num4, total_num4 = coverage.NC(l, threshold=0.7)
+    nc5, activate_num5, total_num5 = coverage.NC(l, threshold=0.9)
+    tknc, pattern_num, total_num6 = coverage.TKNC(l)
+    tknp = coverage.TKNP(l)
+    kmnc, nbc, snac, covered_num, l_covered_num, u_covered_num, neuron_num = coverage.KMNC(l)
+
+    with open(os.path.join(path_to_result, "coverage_result_{}.txt".format(T)), "a") as f:
+        f.write("\n------------------------------------------------------------------------------\n")
+        f.write('NC(0.1): {}  activate_num: {}  total_num: {} \n'.format(nc1, activate_num1, total_num1))
+        f.write('NC(0.3): {}  activate_num: {}  total_num: {} \n'.format(nc2, activate_num2, total_num2))
+        f.write('NC(0.5): {}  activate_num: {}  total_num: {} \n'.format(nc3, activate_num3, total_num3))
+        f.write('NC(0.7): {}  activate_num: {}  total_num: {} \n'.format(nc4, activate_num4, total_num4))
+        f.write('NC(0.9): {}  activate_num: {}  total_num: {} \n'.format(nc5, activate_num5, total_num5))
+        f.write('TKNC: {}  pattern_num: {}  total_num: {} \n'.format(tknc, pattern_num, total_num6))
+        f.write('TKNP: {} \n'.format(tknp))
+        f.write('KMNC: {}  covered_num: {}  total_num: {} \n'.format(kmnc, covered_num, neuron_num))
+        f.write('NBC: {}  l_covered_num: {}  u_covered_num: {} \n'.format(nbc, l_covered_num, u_covered_num))
+        f.write('SNAC: {} \n'.format(snac))
+
 
 def cycle(T: int):
     assert T > 0
@@ -37,17 +73,21 @@ def cycle(T: int):
     nc_number = 0
     for i in range(5000*(T-1), 5000*(T)):
         new_image = mutate(x_train[i])
-        print(i)
-        if i == 10:
+        if i % 100 == 0:
             print('.', end='')
-            break
         if softmax(current_model.predict(np.expand_dims(new_image, axis=0))).argmax(axis=-1) != softmax(current_model.predict(np.expand_dims(x_train[i], axis=0))).argmax(axis=-1):
             # find an adversarial example
             nc_symbol = compare_nc(current_model, x_train, y_train, x_test, y_test, new_image, x_train[i], model_layer)
-            if nc_symbol == True:
-                # new image can cover more neurons
-                nc_index[i] = new_image
-                nc_number += 1
+            if nc_symbol and improve_coverage:
+                # new image can cover more neurons, and we want such improvements
+                    nc_index[i] = new_image
+                    nc_number += 1
+            
+            if (not improve_coverage) and (not nc_symbol):
+                # new image CANNOT cover more neurons, and we want examples cannot improve coverage
+                    nc_index[i] = new_image
+                    nc_number += 1
+
 
     print(nc_number)
     data_folder = 'fuzzing/{}/{}/{}'.format(dataset_name, model_name, is_improve)
@@ -66,6 +106,12 @@ def cycle(T: int):
     retrained_model = retrain(current_model, x_train, y_train, x_test, y_test, batch_size=32, epochs=5)
     new_model_path = "{}{}/{}/{}.h5".format(THIS_MODEL_DIR, dataset_name, model_name, str(T))
     retrained_model.save(new_model_path)
+
+
+    # Step 4. Evaluate the current model
+    ## Evaluate coverage
+    evaluate_coverage(retrained_model, l, T, x_train, y_train, x_test, y_test)
+
 
     print("Done")
     pass
@@ -87,6 +133,8 @@ if __name__ == '__main__':
     order_number = args.order_number
     improve_coverage = args.improve_coverage
 
+    l = [0, model_layer]
+
     is_improve = 'improve' if improve_coverage else 'no_improve'
 
     # Load the original model
@@ -95,6 +143,6 @@ if __name__ == '__main__':
     
     # Save it under this folder
     new_model_path = "{}{}/{}/{}/{}.h5".format(THIS_MODEL_DIR, dataset_name, model_name, is_improve, str(0))
-    model.save(new_model_path)
 
-    cycle(1)
+    for order_number in range(1, 11):
+        cycle(order_number)
