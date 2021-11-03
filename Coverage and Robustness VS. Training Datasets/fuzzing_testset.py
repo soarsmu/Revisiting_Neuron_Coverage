3888b581748c62b2c229d671ae712899c4d6d1be
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2021-11-03 14:14:29 +0800

    feat(RQ1): generate test cases for evaluating robustness

--- Coverage and Robustness VS. Training Datasets/fuzzing_testset.py
+++ Coverage and Robustness VS. Training Datasets/fuzzing_testset.py
@@ -4,6 +4,7 @@ import os
 import argparse
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+MODEL_DIR = "../models/"
 
 ####for solving some specific problems, don't care
 config = tf.compat.v1.ConfigProto()
@@ -33,24 +34,29 @@ if __name__ == '__main__':
 
     # import model
     from keras.models import load_model
-    model = load_model('../data/' + dataset_name + '_data/model/' + model_name + '.h5')
+    model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset_name, model_name)
+    model = load_model(model_path)
     model.summary()
 
     nc_index = {}
     nc_number = 0
-    for i in range(5000*order_number, 5000*(order_number+1)):
-        new_image = mutate(x_test[i])
 
-        if i == 5000*order_number+1000 or i == 5000*order_number+2000 or i == 5000*order_number+3000 or i == 5000*order_number+4000:
-            print("-------------------------------------THIS IS {}-------------------------------------".format(i))
-        if softmax(model.predict(np.expand_dims(new_image, axis=0))).argmax(axis=-1) != softmax(model.predict(np.expand_dims(x_test[i], axis=0))).argmax(axis=-1):
-            nc_symbol = compare_nc(model, x_train, y_train, x_test, y_test, new_image, x_test[i], model_layer)
-            if nc_symbol == True:
-                nc_index[i] = new_image
-                nc_number += 1
+    store_path = 'new_test/{}/{}'.format(dataset_name, model_name)
+    os.makedirs(store_path, exist_ok=True)
+    
+    for order_number in range(2):
+        for i in range(5000*order_number, 5000*(order_number+1)):
+            new_image = mutate(x_test[i])
+            if i == 5000*order_number+1000 or i == 5000*order_number+2000 or i == 5000*order_number+3000 or i == 5000*order_number+4000:
+                print("-------------------------------------THIS IS {}-------------------------------------".format(i))
+            if softmax(model.predict(np.expand_dims(new_image, axis=0))).argmax(axis=-1) != softmax(model.predict(np.expand_dims(x_test[i], axis=0))).argmax(axis=-1):
+                nc_symbol = compare_nc(model, x_train, y_train, x_test, y_test, new_image, x_test[i], model_layer)
+                if nc_symbol == True:
+                    nc_index[i] = new_image
+                    nc_number += 1
 
-    print(nc_number)
-    np.save('fuzzing/nc_index_test_{}.npy'.format(order_number), nc_index)
+        print(nc_number)
+        np.save(os.path.join(store_path, 'nc_index_test_{}.npy'.format(order_number)), nc_index)
 
 
 
