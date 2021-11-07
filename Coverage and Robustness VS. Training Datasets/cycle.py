b5af3bfb6c32b29419441f2253641a3c2bc01fbc
jiekeshi <jiekeshi125@gmail.com>
2021-11-07 14:55:33 +0800

    feat: add the whole pipeline for RQ1

--- Coverage and Robustness VS. Training Datasets/cycle.py
+++ Coverage and Robustness VS. Training Datasets/cycle.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 import numpy as np
 from keras.models import load_model
 import os
+from tqdm import tqdm
 
 
 DATA_DIR = "../data/"
@@ -34,7 +35,7 @@ def evaluate_robustness(T, retrained_model, x_test, y_test, x_test_new):
     _, _, RGB = criteria.robust_gaussian_blur()
     _, _, RIC = criteria.robust_image_compression(1)
 
-    with open(os.path.join(path_to_result, "robustness_metrics_{}.txt".format(T)), "a") as f:
+    with open(os.path.join(path_to_result, "robustness_metrics_{}.txt".format(T)), "w") as f:
         f.write("\n------------------------------------------------------------------------------\n")
         f.write('the result of {} {} is: \n'.format(dataset_name, model_name))
         f.write('MR: {} \n'.format(MR))
@@ -57,22 +58,34 @@ def evaluate_coverage(model, l, T, x_train, y_train, x_test, y_test):
     coverage = Coverage(model, x_train, y_train, x_test, y_test, x_train)
     # Based on the original code to evaluate Coverage, x_train and x_train_adv is the same.
     # In the Coverage class, only the last parameter is used.
-    nc1, activate_num1, total_num1 = coverage.NC(l, threshold=0.1)
-    nc2, activate_num2, total_num2 = coverage.NC(l, threshold=0.3)
-    nc3, activate_num3, total_num3 = coverage.NC(l, threshold=0.5)
-    nc4, activate_num4, total_num4 = coverage.NC(l, threshold=0.7)
-    nc5, activate_num5, total_num5 = coverage.NC(l, threshold=0.9)
-    tknc, pattern_num, total_num6 = coverage.TKNC(l)
-    tknp = coverage.TKNP(l)
-    kmnc, nbc, snac, covered_num, l_covered_num, u_covered_num, neuron_num = coverage.KMNC(l)
+    nc, activate_num, total_num = coverage.NC(l, threshold=[0.1, 0.3, 0.5, 0.7, 0.9])
+    # nc2, activate_num2, total_num2 = coverage.NC(l, threshold=0.3)
+    # nc3, activate_num3, total_num3 = coverage.NC(l, threshold=0.5)
+    # nc4, activate_num4, total_num4 = coverage.NC(l, threshold=0.7)
+    # nc5, activate_num5, total_num5 = coverage.NC(l, threshold=0.9)
+    for threshold in nc.keys():
+        print('NC({}): {}  activate_num: {}  total_num: {}'.format(threshold, nc[threshold], activate_num[threshold], total_num))
 
-    with open(os.path.join(path_to_result, "coverage_result_{}.txt".format(T)), "a") as f:
+    tknc, pattern_num, total_num6 = coverage.TKNC(l)
+    print('TKNC: {}  pattern_num: {}  total_num: {}'.format(tknc, pattern_num, total_num6))
+    
+    tknp = coverage.TKNP(l)
+    print('TKNP: {}'.format(tknp))
+        
+    kmnc, nbc, snac, covered_num, l_covered_num, u_covered_num, neuron_num = coverage.KMNC(l)
+    print('KMNC: {}  covered_num: {}  total_num: {}'.format(kmnc, covered_num, neuron_num))
+    print('NBC: {}  l_covered_num: {}  u_covered_num: {}'.format(nbc, l_covered_num, u_covered_num))
+    print('SNAC: {}'.format(snac))
+
+    with open(os.path.join(path_to_result, "coverage_result_{}.txt".format(T)), "w") as f:
         f.write("\n------------------------------------------------------------------------------\n")
-        f.write('NC(0.1): {}  activate_num: {}  total_num: {} \n'.format(nc1, activate_num1, total_num1))
-        f.write('NC(0.3): {}  activate_num: {}  total_num: {} \n'.format(nc2, activate_num2, total_num2))
-        f.write('NC(0.5): {}  activate_num: {}  total_num: {} \n'.format(nc3, activate_num3, total_num3))
-        f.write('NC(0.7): {}  activate_num: {}  total_num: {} \n'.format(nc4, activate_num4, total_num4))
-        f.write('NC(0.9): {}  activate_num: {}  total_num: {} \n'.format(nc5, activate_num5, total_num5))
+        f.write('the coverage of {} {} is: \n'.format(dataset_name, model_name))
+        for threshold in nc.keys():
+            f.write('NC({}): {}  activate_num: {}  total_num: {} \n'.format(threshold, nc[threshold], activate_num[threshold], total_num))
+        # f.write('NC(0.3): {}  activate_num: {}  total_num: {} \n'.format(nc[0.3], activate_num[0.3], total_num))
+        # f.write('NC(0.5): {}  activate_num: {}  total_num: {} \n'.format(nc[0.5], activate_num[0.5], total_num))
+        # f.write('NC(0.7): {}  activate_num: {}  total_num: {} \n'.format(nc[0.7], activate_num[0.7], total_num))
+        # f.write('NC(0.9): {}  activate_num: {}  total_num: {} \n'.format(nc[0.9], activate_num[0.9], total_num))
         f.write('TKNC: {}  pattern_num: {}  total_num: {} \n'.format(tknc, pattern_num, total_num6))
         f.write('TKNP: {} \n'.format(tknp))
         f.write('KMNC: {}  covered_num: {}  total_num: {} \n'.format(kmnc, covered_num, neuron_num))
@@ -90,7 +103,15 @@ def cycle(T: int):
     # Step 2. According to the current M_i and dataset, generate examples T_i
     ## Load the current dataset we have
     x_train, y_train, x_test, y_test = load_data(dataset_name)
-    for i in range(T-1):
+
+    
+    print("Log: Start do transformation in images")
+    new_images = []
+
+    for i in tqdm(range(len(x_train))):
+        new_images.append(mutate(x_train[i]))
+
+    for i in range(1, T):
         index = np.load('fuzzing/{}/{}/{}/nc_index_{}.npy'.format(dataset_name, model_name, is_improve, i), allow_pickle=True).item()
         for y, x in index.items():
             x_train = np.concatenate((x_train, np.expand_dims(x, axis=0)), axis=0)
@@ -99,25 +120,23 @@ def cycle(T: int):
     ## Generate new examples
     nc_index = {}
     nc_number = 0
-    for i in range(5000*(T-1), 5000*(T)):
-        new_image = mutate(x_train[i])
-        if i % 100 == 0:
-            print('.', end='')
-        if softmax(current_model.predict(np.expand_dims(new_image, axis=0))).argmax(axis=-1) != softmax(current_model.predict(np.expand_dims(x_train[i], axis=0))).argmax(axis=-1):
+    for i in tqdm(range(5000*(T-1), 5000*(T), 500), desc="Total progress:"):
+        for index, (pred_new, pred_old) in enumerate(zip(softmax(current_model.predict(np.array(new_images[i:i+500]))).argmax(axis=-1), softmax(current_model.predict(x_train[i:i+500])).argmax(axis=-1))):
             # find an adversarial example
-            nc_symbol = compare_nc(current_model, x_train, y_train, x_test, y_test, new_image, x_train[i], model_layer)
-            if nc_symbol and improve_coverage:
-                # new image can cover more neurons, and we want such improvements
-                    nc_index[i] = new_image
-                    nc_number += 1
-            
-            if (not improve_coverage) and (not nc_symbol):
-                # new image CANNOT cover more neurons, and we want examples cannot improve coverage
-                    nc_index[i] = new_image
-                    nc_number += 1
+            if pred_new != pred_old:
+                nc_symbol = compare_nc(current_model, x_train, y_train, x_test, y_test, new_images[i+index], x_train[i+index], model_layer)
+                if nc_symbol and improve_coverage:
+                    # new image can cover more neurons, and we want such improvements
+                        nc_index[i] = new_images[i+index]
+                        nc_number += 1
+                
+                if (not improve_coverage) and (not nc_symbol):
+                    # new image CANNOT cover more neurons, and we want examples cannot improve coverage
+                        nc_index[i] = new_images[i+index]
+                        nc_number += 1
 
 
-    print(nc_number)
+    print("Log: new image can/cannot cover more neurons: {}".format(nc_number))
     data_folder = 'fuzzing/{}/{}/{}'.format(dataset_name, model_name, is_improve)
     os.makedirs(data_folder, exist_ok=True)
     np.save(os.path.join(data_folder, 'nc_index_{}.npy'.format(T)), nc_index)
@@ -138,14 +157,16 @@ def cycle(T: int):
 
     # Step 4. Evaluate the current model
     ## Evaluate coverage
+    print("\nEvaluate coverage ......")
     evaluate_coverage(retrained_model, l, T, x_train, y_train, x_test, y_test)
 
     ## Evaluate robustness
+    print("\nEvaluate robustness ......")
     store_path = 'new_test/{}/{}'.format(dataset_name, model_name)
     x_test_new = np.load(os.path.join(store_path, 'x_test_new.npy'))
     evaluate_robustness(T, retrained_model, x_test, y_test, x_test_new)
 
-    print("Done")
+    print("Done\n")
 
 
 
@@ -153,26 +174,40 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", default='lenet1', type=str)
     parser.add_argument("--dataset", default='mnist', type=str)
-    parser.add_argument("--model_layer", default=8, type=int)
-    parser.add_argument("--order_number", default=0, type=int)
     parser.add_argument("--improve_coverage", action='store_true')
 
     args = parser.parse_args()
     model_name = args.model_name
-    model_layer = args.model_layer
     dataset_name = args.dataset
-    order_number = args.order_number
     improve_coverage = args.improve_coverage
-
 
     is_improve = 'improve' if improve_coverage else 'no_improve'
 
     # Load the original model
     model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset_name, model_name)
     model = load_model(model_path)
-    model_layer = len(model.layers) - 1
-    l = [0, model_layer]
+    model_layer = len(model.layers)
+    l = range(model_layer)
+
+    x_train, y_train, x_test, y_test = load_data(dataset_name)
     
+    new_images = []
+    for i in tqdm(range(len(x_test)), desc="transformation ......"):
+        new_images.append(mutate(x_test[i]))
+
+    nc_index = {}
+    nc_number = 0
+    for i in tqdm(range(0, len(x_test), 500), desc="Total progress:"):
+        for index, (pred_new, pred_old) in enumerate(zip(softmax(model.predict(np.array(new_images[i:i+500]))).argmax(axis=-1), softmax(model.predict(x_test[i:i+500])).argmax(axis=-1))):
+            nc_symbol = compare_nc(model, x_train, y_train, x_test, y_test, new_images[i+index], x_test[i+index], model_layer)
+            if nc_symbol == True:
+                nc_index[i+index] = new_images[i+index]
+                nc_number += 1
+    
+    print("Log: new image can cover more neurons: {}".format(nc_number))
+    store_path = 'new_test/{}/{}'.format(dataset_name, model_name)
+    np.save(os.path.join(store_path, 'x_test_new.npy'), nc_index)
+
     # Save it under this folder
     new_model_path = "{}{}/{}/{}/{}.h5".format(THIS_MODEL_DIR, dataset_name, model_name, is_improve, str(0))
     model.save(new_model_path)
