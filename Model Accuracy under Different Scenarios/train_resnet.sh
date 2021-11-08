77f337351e76c91ce04021c5f1cd39d43ce64d52
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-11-08 08:18:05 +0800

    chore(): script to train resnet models

---
+++ Model Accuracy under Different Scenarios/train_resnet.sh
@@ -0,0 +1,11 @@
+## train resnet56 on cifar10
+python train_resnet.py --model_name resnet56 --dataset cifar10 --num_classes 10
+
+## train resnet56 on cifar10
+python train_resnet.py --model_name resnet56 --dataset eurosat --num_classes 10
+
+## train resnet32 on eurosat
+python train_resnet.py --model_name resnet32 --dataset eurosat --num_classes 10
+
+## train resnet20 on eurosat
+python train_resnet.py --model_name resnet20 --dataset eurosat --num_classes 10
