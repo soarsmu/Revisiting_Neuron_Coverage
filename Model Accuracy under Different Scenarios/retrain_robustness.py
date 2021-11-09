9068d0ce422dd45cacf6f01a01f6f2f6d72fd70a
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-11-10 00:05:47 +0800

    refactor(): restructure folders and run fuzzing

--- Model Accuracy under Different Scenarios/retrain_robustness.py
+++ Model Accuracy under Different Scenarios/retrain_robustness.py
@@ -17,42 +17,68 @@ from helper import load_data, retrain
 
 import tensorflow as tf
 import os
+from keras.models import load_model
 
 
-DATA_DIR = "../data/"
-MODEL_DIR = "../models/"
+sys.path.append('..')
 
+import parameters as param
 
-
+from fuzzing import run_fuzzing
 
 if __name__ == '__main__':
 
-    dataset = 'mnist'
-    model_name = 'lenet1'
+    # dataset = 'mnist'
+    # model_name = 'lenet1'
 
-    datasets = ['mnist', 'cifar', 'svhn']
-    model_dict = {
-                'mnist': ['lenet1', 'lenet4', 'lenet5'],
-                'cifar': ['vgg16'], # , 'resnet20'
-                'svhn' : ['svhn_model', 'svhn_second', 'svhn_first']
-                }
+    # datasets = ['mnist', 'cifar', 'svhn']
+    # model_dict = {
+    #             'mnist': ['lenet1', 'lenet4', 'lenet5'],
+    #             'cifar': ['vgg16'], # , 'resnet20'
+    #             'svhn' : ['svhn_model', 'svhn_second', 'svhn_first']
+    #             }
     
-    from keras.models import load_model
-    for dataset in datasets:
-        for model_name in model_dict[dataset]:
-            x_train, y_train, x_test, y_test = load_data(dataset)
-            # ## load mine trained model
-            model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset, model_name)
+    datasets = ['cifar', 'eurosat']
+    model_dict = {
+        'cifar': ['resnet56'],
+        'eurosat': ['resnet20', 'resnet56'],
+    }
+    
+    for dataset_name in datasets:
+        for model_name in model_dict[dataset_name]:
+
+            print("Dataset: ", dataset_name)
+            print("Model: ", model_name)
+
+            x_train, y_train, x_test, y_test = load_data(dataset_name)
+            
+            # load trained model
+            model_path = "{}{}/{}.h5".format(param.MODEL_DIR, dataset_name, model_name)
             model = load_model(model_path)
-            model.summary()
-            folder_to_store = 'fuzzing/{}/{}/'.format(dataset, model_name)
+            # model.summary()
+            
+            # get model layer
+            model_layer = len(model.layers) - 1
+
+            # prepare folder to store images generated from fuzzing
+            folder_to_store = '{}{}/fuzzing/{}/'.format(param.DATA_DIR, dataset_name, model_name)
+            os.makedirs(folder_to_store, exist_ok=True)
+
+            # run fuzzing 
+            run_fuzzing(dataset_name, model, x_train, y_train, x_test,
+                        y_test, model_layer, folder_to_store)
+            
+            # load images generated from fuzzing
             index = np.load(folder_to_store + 'nc_index_{}.npy'.format(4), allow_pickle=True).item()
             for y, x in index.items():
                 x_train = np.concatenate((x_train, np.expand_dims(x, axis=0)), axis=0)
                 y_train = np.concatenate((y_train, np.expand_dims(y_train[y], axis=0)), axis=0)
 
+            # retrain model using augmented data from fuzzing
             retrained_model = retrain(model, x_train, y_train, x_test, y_test, batch_size=256, epochs=60)
-            retrained_model.save('new_model/dp_{}.h5'.format(model_name))
+            
+            # save model for future usage
+            retrained_model.save('{}{}/adv_{}_deephunter.h5'.format(param.MODEL_DIR, dataset_name, model_name))
 
 
 
