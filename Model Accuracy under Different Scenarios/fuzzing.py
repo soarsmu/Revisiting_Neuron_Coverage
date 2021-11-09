c19279239db3db059d2cad942f148e3c4bd66984
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-11-10 00:02:54 +0800

    refactor(): create fuzzing function to reduce redundancy

--- Model Accuracy under Different Scenarios/fuzzing.py
+++ Model Accuracy under Different Scenarios/fuzzing.py
@@ -1,36 +1,61 @@
+from helper import mutate, load_data, softmax, compare_nc, check_data_path
+from keras.models import load_model
 import numpy as np
 
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
 import os
-
-DATA_DIR = "../data/"
-MODEL_DIR = "../models/"
-
-
-
-
 import argparse
-import os
+import os, sys
 
 import shutil
 import warnings
 from helper import Coverage
-from helper import mutate, load_data, softmax, compare_nc, check_data_path
 
 warnings.filterwarnings("ignore")
 
+sys.path.append('..')
+
+import parameters as param
+
+
+def run_fuzzing(dataset_name, model, x_train, y_train, x_test, y_test, model_layer, folder_to_store, order_numbers=10):
+    for order_number in range(0, order_numbers):
+        nc_index = {}
+        nc_number = 0
+        for i in range(3000*order_number, 3000*(order_number+1)):
+            new_image = mutate(x_train[i], dataset_name)
+
+            if i == 5000*order_number+1000 or i == 5000*order_number+3000:
+                print("-------------------------------------THIS IS {}-------------------------------------".format(i))
+            if softmax(model.predict(np.expand_dims(new_image, axis=0))).argmax(axis=-1) != softmax(model.predict(np.expand_dims(x_train[i], axis=0))).argmax(axis=-1):
+
+                nc_symbol = compare_nc(
+                    model, x_train, y_train, x_test, y_test, new_image, x_train[i], model_layer)
+
+                if nc_symbol == True:
+                    nc_index[i] = new_image
+                    nc_number += 1
+
+        print(nc_number)
+        np.save(folder_to_store +
+                'nc_index_{}.npy'.format(order_number), nc_index)
 
 
 if __name__ == '__main__':
 
-    datasets = ['mnist', 'cifar', 'svhn']
+    # datasets = ['mnist', 'cifar', 'svhn']
+    # model_dict = {
+    #             'mnist': ['lenet1', 'lenet4', 'lenet5'],
+    #             'cifar': ['vgg16', 'resnet20'], 
+    #             'svhn' : ['svhn_model', 'svhn_second', 'svhn_first']
+    #             }
+
+    datasets = ['cifar']
     model_dict = {
-                'mnist': ['lenet1', 'lenet4', 'lenet5'],
-                'cifar': ['vgg16', 'resnet20'], 
-                'svhn' : ['svhn_model', 'svhn_second', 'svhn_first']
-                }
+        'cifar': ['resnet56']
+    }
 
     # Check path
     for dataset_name in model_dict.keys():
@@ -38,45 +63,27 @@ if __name__ == '__main__':
         check_data_path(dataset_name)
         # verify model path
         for model_name in model_dict[dataset_name]:
-            model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset_name, model_name)
+            model_path = "{}{}/{}.h5".format(param.MODEL_DIR, dataset_name, model_name)
             assert os.path.exists(model_path)
 
-    from keras.models import load_model
-
-    for dataset in datasets:
-        for model_name in model_dict[dataset]:
+    
+    for dataset_name in datasets:
+        for model_name in model_dict[dataset_name]:
             # load original model
-            model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset, model_name)
+            model_path = "{}{}/{}.h5".format(param.MODEL_DIR, dataset_name, model_name)
             model = load_model(model_path)
             # get model layer
             model_layer = len(model.layers) - 1
 
             # load dataset
-            x_train, y_train, x_test, y_test = load_data(dataset)
+            x_train, y_train, x_test, y_test = load_data(dataset_name)
 
+            ### save data
+            folder_to_store = '{}{}/fuzzing/{}/'.format(param.DATA_DIR, dataset_name, model_name)
+            os.makedirs(folder_to_store, exist_ok=True)
 
-            for order_number in range(0, 10):
-                nc_index = {}
-                nc_number = 0
-                for i in range(3000*order_number, 3000*(order_number+1)):
-                    new_image = mutate(x_train[i], dataset)
+            run_fuzzing(model, x_train, y_train, x_test,y_test, model_layer, folder_to_store)
 
-                    if i == 5000*order_number+1000 or i == 5000*order_number+3000:
-                        print("-------------------------------------THIS IS {}-------------------------------------".format(i))
-                    if softmax(model.predict(np.expand_dims(new_image, axis=0))).argmax(axis=-1) != softmax(model.predict(np.expand_dims(x_train[i], axis=0))).argmax(axis=-1):
-
-                        nc_symbol = compare_nc(model, x_train, y_train, x_test, y_test, new_image, x_train[i], model_layer)
-
-                        if nc_symbol == True:
-                            nc_index[i] = new_image
-                            nc_number += 1
-
-                print(nc_number)
-                
-                ### save data
-                folder_to_store = 'fuzzing/{}/{}/'.format(dataset, model_name)
-                os.makedirs(folder_to_store, exist_ok=True)
-                np.save(folder_to_store +  '/nc_index_{}.npy'.format(order_number), nc_index)
 
 
 
