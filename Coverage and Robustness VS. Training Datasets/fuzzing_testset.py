d6c6273663f3f43e1fb6b957405993d781eb4499
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-11-16 20:20:24 +0800

    feat(): refactpr the folder

--- Coverage and Robustness VS. Training Datasets/fuzzing_testset.py
+++ Coverage and Robustness VS. Training Datasets/fuzzing_testset.py
@@ -1,45 +1,55 @@
+import sys
+sys.path.append('..')
+import parameters as param
+
 import numpy as np
 import tensorflow as tf
 import os
 import argparse
 
 from tqdm import tqdm
-
-MODEL_DIR = "../models/"
-
+from keras.models import load_model
+    
 ####for solving some specific problems, don't care
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config=config)
 import warnings
-from helper import load_data, softmax, compare_nc, mutate
+from helper import load_data, softmax, compare_nc, mutate, differentiable_mutate, nondifferentiable_mutate
 
 warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", default='lenet1', type=str)
     parser.add_argument("--dataset", default='mnist', type=str)
+    parser.add_argument("--model", default='lenet1', type=str)
+    parser.add_argument("--mutation", default='deephunter', type=str, choices=['deephunter', 'differentiable','nondifferentiable'])
 
     args = parser.parse_args()
 
-    model_name = args.model_name
+    model_name = args.model
     dataset_name = args.dataset
 
     # load dataset
     x_train, y_train, x_test, y_test = load_data(dataset_name)
 
     # import model
-    from keras.models import load_model
-    model_path = "{}{}/{}.h5".format(MODEL_DIR, dataset_name, model_name)
+    model_path = "{}{}/{}.h5".format(param.MODEL_DIR, dataset_name, model_name)
     model = load_model(model_path)
     model_layer = len(model.layers) - 1
 
-    store_path = 'new_test/{}/{}'.format(dataset_name, model_name)
+    store_path = "{}{}/adv/{}/fuzzing_{}/".format(param.DATA_DIR, dataset_name, model_name, args.mutation)
+    
     os.makedirs(store_path, exist_ok=True)
     new_images = []
     for i in tqdm(range(len(x_test)), desc="transformation ......"):
-        new_images.append(mutate(x_test[i]))
+        if args.mutation == "deephunter" :
+            new_images.append(mutate(x_train[i]))
+        elif args.mutation == "differentiable" : 
+            new_images.append(differentiable_mutate(x_train[i]))
+        elif args.mutation == "nondifferentiable" : 
+            new_images.append(nondifferentiable_mutate(x_train[i]))
+
 
     for order_number in range(2):
         nc_index = {}
@@ -57,10 +67,9 @@ if __name__ == '__main__':
     for order_number in range(2):
         index = np.load(os.path.join(store_path, 'nc_index_test_{}.npy'.format(order_number)), allow_pickle=True).item()
         for y, x in index.items():
-            # print(y)
             x_test[y] = x
 
-    np.save(os.path.join(store_path, 'x_test_new.npy'), x_test)
+    np.save(os.path.join(store_path, 'x_test.npy'), x_test)
 
 
 
